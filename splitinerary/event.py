99a5a20f07c81f6bf4685a0301f4cbf8072f772d
el3030 <el3030@columbia.edu>
2023-04-04 20:30:04 -0400

    added documentation for splitinerary objects

--- splitinerary/event.py
+++ splitinerary/event.py
@@ -1,14 +1,21 @@
 class Event:
-    """
-    Event object. Sortable by datetime.
+    """Event object. Each event is sortable among other Events by datetime.
 
     Attributes:
-        datetime: datetime.datetime object of when the user wants to arrive at
-        the event, NOT NECESSARILY when the event starts (e.g. user might
-        arrive at the airport 2 hours before the flight departs)
+        datetime (datetime.datetime): when the user wants to arrive at
+            the event, NOT NECESSARILY when the event starts (e.g. user might
+            arrive at the airport 2 hours before the flight departs).
     """
 
     def __init__(self, datetime, users=None):
+        """Inits Event.
+
+        Args:
+            datetime (datetime.Datetime): datetime.Datetime object of when the
+                user gets to the event.
+            users (list of User, optional): Users participating in the Event.
+                Defaults to None.
+        """
         self.datetime = datetime
         if users is None:
             self.users = []
@@ -16,15 +23,36 @@ class Event:
             self.users = users
 
     def add_user(self, user):
+        """Add user to Event.
+
+        Args:
+            user (User): User to add to Event.
+        """
         self.users.append(user)
 
     def get_users(self):
+        """Get Users participating in Event.
+
+        Returns:
+            list of User: Users participating in Event if there are any,
+                else None.
+        """
         return self.users if self.users else None
 
     def get_date(self):
+        """Get date of Event.
+
+        Returns:
+            datetime.Date: Date the event takes place on.
+        """
         return self.datetime.date()
 
     def get_time(self):
+        """Get time of Event (without the date).
+
+        Returns:
+            datetime.Time: Time the event takes place on.
+        """
         return self.datetime.time()
 
     def __lt__(self, other):
